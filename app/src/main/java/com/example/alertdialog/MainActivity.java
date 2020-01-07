d8383a0574e661c4089294ee9efee5accd85f524
rohitohlyan <52256826+rohitohlyan@users.noreply.github.com>
2020-01-07 12:25:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+        Button btn;
+        AlertDialog.Builder builder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button2);
+        builder=new AlertDialog.Builder(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.setMessage("Do you want to close the application")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                AlertDialog alert=builder.create();
+                alert.setTitle("Alert Dialog Example");
+                alert.show();
+            }
+        });
+
+    }
+}
